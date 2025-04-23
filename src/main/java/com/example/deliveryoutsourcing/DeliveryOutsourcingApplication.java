abcb7f38e189b599e9b8e710d8eb79f863adb107
고나은 <gonaeunn@gmail.com>
2025-04-23 12:16:07 +0900

    first commit

---
+++ src/main/java/com/example/deliveryoutsourcing/DeliveryOutsourcingApplication.java
@@ -0,0 +1,13 @@
+package com.example.deliveryoutsourcing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryOutsourcingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryOutsourcingApplication.class, args);
+    }
+
+}
